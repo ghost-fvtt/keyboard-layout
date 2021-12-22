8bb571884349ff1f0e4dc8427e11b7d113915a41
Martin Ottingius Karlsson <martin810907@gmail.com>
2021-12-22 15:44:15 +0100

    Added support for Swedish
    Translated module and added support for Swedish QWERTY

--- src/module/keyboard-layouts/index.js
+++ src/module/keyboard-layouts/index.js
@@ -1,11 +1,14 @@
 // SPDX-FileCopyrightText: 2021 Johannes Loher
+// SPDX-FileCopyrightText: 2021 Martin (fohswe#8355), GH: plutoneld
 //
 // SPDX-License-Identifier: MIT
 
 import { us } from './us.js';
 import { deLatin1 } from './de-latin1.js';
+import { svLatin } from './sv-latin.js';
 
 export const keyboardLayouts = {
   us,
   deLatin1,
+  svLatin,
 };
