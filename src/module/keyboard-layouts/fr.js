08605beed84f0566349d4bd61fe15959e007f2c9
Max Van Sande <mxvs112@gmail.com>
2021-12-26 16:10:54 +0100

    feat: add support for French (AZERTY)

---
+++ src/module/keyboard-layouts/fr.js
@@ -0,0 +1,42 @@
+// SPDX-FileCopyrightText: 2021 Max VS (Maximus#6001)
+//
+// SPDX-License-Identifier: MIT
+
+export const fr = {
+  keycodeDisplayMapping: {
+    KeyW: 'Z',
+    KeyZ: 'W',
+    KeyQ: 'A',
+    KeyA: 'Q',
+    KeyM: ',',
+    Minus: ')',
+    Equal: '=',
+    BracketRight: '$',
+    BracketLeft: '^',
+    Digit0: 'à',
+    Digit1: '&',
+    Digit2: 'é',
+    Digit3: '"',
+    Digit4: "'",
+    Digit5: '(',
+    Digit6: '-',
+    Digit7: 'è',
+    Digit8: '_',
+    Digit9: 'ç',
+    Semicolon: 'M',
+    Period: ':',
+    Backquote: '²',
+    Quote: 'ù',
+    IntlBackslash: '<',
+    Backslash: '*',
+    Slash: '!',
+    Comma: ';',
+  },
+
+  keybindingMapping: {
+    KeyZ: 'KeyW',
+    Minus: 'Digit6',
+  },
+
+  i18n: 'KEYBOARDLAYOUT.KeyboardLayoutFr',
+};
--- src/module/keyboard-layouts/index.js
+++ src/module/keyboard-layouts/index.js
@@ -1,14 +1,17 @@
 // SPDX-FileCopyrightText: 2021 Johannes Loher
 // SPDX-FileCopyrightText: 2021 Martin (fohswe#8355), GH: plutoneld
+// SPDX-FileCopyrightText: 2021 Max VS (Maximus#6001)
 //
 // SPDX-License-Identifier: MIT
 
 import { us } from './us.js';
 import { deLatin1 } from './de-latin1.js';
 import { svLatin1 } from './sv-latin1.js';
+import { fr } from './fr.js';
 
 export const keyboardLayouts = {
   us,
   deLatin1,
   svLatin1,
+  fr,
 };
