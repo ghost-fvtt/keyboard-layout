f9b15d1b0748941ca46a031bb214714930811b77
szilajka <szilajka1@gmail.com>
2025-04-09 00:43:23 +0200

    feat: add hungarian layout

---
+++ src/keyboard-layouts/hu.js
@@ -0,0 +1,26 @@
+export const hu = {
+  keycodeDisplayMapping: {
+    Backquote: '0',
+    Digit0: 'Ö',
+    Minus: 'Ü',
+    Equal: 'Ó',
+    KeyY: 'Z',
+    BracketLeft: 'Ő',
+    BracketRight: 'Ú',
+    Semicolon: 'É',
+    Quote: 'Á',
+    Backslash: 'Ű',
+    IntlBackslash: 'Í',
+    KeyZ: 'Y',
+    Slash: '-',
+  },
+
+  keybindingMapping: {
+    KeyY: 'KeyZ',
+    KeyZ: 'KeyY',
+    Digit0: 'Backquote',
+    Minus: 'Slash',
+  },
+
+  i18n: 'KEYBOARDLAYOUT.KeyboardLayoutHu',
+};
